e528d0cba382e808a2838a71e93a47e6a95aa2e8
sjwinther <sebastianwinther@gmail.com>
2018-02-26 16:57:41 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # portfolio-vue
+
+> A Vue.js portfolio
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" class="min-h-screen flex items-center justify-center">
+    <p class="text-5xl">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      message: 'Hello portfolio.'
+    };
+  }
+};
+</script>
+
+<style>
+@tailwind preflight;
+@tailwind utilities;
+
+#app {
+  @apply .font-sans .antialiased .text-black;
+}
+</style>
