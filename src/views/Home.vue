02f34d92e28f958df04324b0433c3daf0ba991bb
sjwinther <sebastianwinther@gmail.com>
2018-05-10 16:38:52 +0200

    itty-bitty margin changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="fade-in">
     <div class="py-6 sm:py-12">
       <h1>Hi, I'm Sebastian</h1>
-      <p class="mb-12">I'm a student at the Digital Design and Communication MSc programme at the IT-University of Copenhagen. I enjoy designing digital interfaces and working with user experiences. I'm motivated by building stuff and learning from it.</p>
+      <p class="-mt-4 sm:mt-0 mb-8 sm:mb-12">I'm a student at the Digital Design and Communication MSc programme at the IT-University of Copenhagen. I enjoy designing digital interfaces and working with user experiences. I'm motivated by building stuff and learning from it.</p>
       <p>I'm available for work. Do you want to get in touch? <a href="mailto:sebastianwinther@gmail.com?subject=Let's get coffee" class="inline-block inline-link"><span class="mr-1">→</span>Write me an email</a>.</p>
     </div>
     <div v-if="projects" class="fade-in py-6 sm:py-12">
