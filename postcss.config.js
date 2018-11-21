3ad66ad4c6003bfaaf72c3b4c1545e353758d386
Sebastian Winther <sebastianwinther@gmail.com>
2018-11-21 13:53:27 +0100

    purgecss + cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,22 @@
-var tailwindcss = require("tailwindcss");
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
 
 module.exports = {
-  plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")]
+  plugins: [
+    require("tailwindcss")("./tailwind.config.js"),
+    require("@fullhuman/postcss-purgecss")({
+      content: ["./src/**/*.vue"],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ["vue"]
+        }
+      ]
+    }),
+    require("autoprefixer"),
+    require("cssnano")
+  ]
 };
